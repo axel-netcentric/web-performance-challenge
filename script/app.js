0edf9aee47e25d71bbd080990018c3b68c1e6775
Áxel Costas <axel.costas@netcentric.biz>
2023-03-16 15:49:29 +0100

    Make weather widget async

--- script/app.js
+++ script/app.js
@@ -58,9 +58,26 @@ const cookieLayerInit = () => {
   document.body.classList.add('no-scroll');
 };
 
+const loadWeatherWidget = () => {
+  document.addEventListener("DOMContentLoaded", function (event) {
+    setTimeout(() => {
+      // !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+      var d=document,s='script',id='weatherwidget-io-js';
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (!d.getElementById(id)) {
+        js = d.createElement(s);
+        js.id = id;
+        js.src = 'https://weatherwidget.io/js/widget.min.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }
+    }, 3500);
+  });
+}
+
 const initApp = () => {
   dynamicContent();
   cookieLayerInit();
+  loadWeatherWidget();
 };
 
 initApp();
